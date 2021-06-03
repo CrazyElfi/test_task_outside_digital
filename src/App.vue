71b6ae366df0c5ab7e39512bd22be2b7fbf81d98
elfi <alrigaff@gmail.com>
2021-06-04 00:43:19 +0300

    added web-layout

---
+++ src/App.vue
@@ -0,0 +1,180 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <!--First model-->
+      <button
+          class="btn"
+          @click="modalFirst = !modalFirst">
+        Налоговый вычет
+      </button>
+      <modals
+          title="First modal"
+          v-show="modalFirst"
+          @close="modalFirst = false">
+        <div slot="body" class="body-wrapper">
+          <p class="text-info">
+            Используйте налоговый вычет чтобы погасить ипотеку досрочно. Размер налогового вычета составляет не более
+            13% от своего официального годового дохода.
+          </p>
+          <label class="title">Ваша зарплата в месяц</label>
+          <input type="number"
+                 class="salary"
+                 v-model="salary"
+                 placeholder="Введите данные">
+          <!--                 @change="inputSalary"-->
+
+          <button class="title calc"
+                  @click="calcSizePayoff">
+            Рассчитать
+          </button>
+
+          <div class="calculation hidden">
+            <p class="title">Итого можете внести в качестве досрочных:</p>
+
+            <div class="checkboxes__wrapper">
+              <div
+                  v-for="(payoff,index) in arrPayoff"
+                  :key="index"
+                  :id="payoff.year"
+                  class="checkboxes-item">
+
+
+                <input name="checkboxPayoff"
+                       type="checkbox"
+                       v-model="checkboxValue[index]"
+                >
+                <label>
+                  <span> {{ payoff.sumPayoff }} рублей </span>
+                  в {{ payoff.year }}{{ payoff.ending }} год
+                </label>
+              </div>
+
+
+            </div>
+          </div>
+
+          <div class="switch__wrapper">
+            <p class="title">Что уменьшаем?</p>
+            <button class="switch active">Платёж</button>
+            <button class="switch">Срок</button>
+          </div>
+
+
+          <button
+              class="btnPrimary"
+              @click="modalFirst = false">
+            Добавить
+          </button>
+        </div>
+      </modals>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import modals from '@/components/Modal.vue'
+
+const maxSizePayoff = 260000
+export default {
+  name: 'App',
+  components: {
+    modals
+  },
+  data() {
+    return {
+      modalFirst: false,
+      checkboxValue:[],
+      salary: '',
+      arrPayoff: [],
+      years: null
+    }
+  },
+  methods: {
+    submitSecondForm() {
+      // axios
+      //     .post('http://localhost:8080/...', {
+      //       name: this.modalSecond.name,
+      //       email: this.modalSecond.email
+      //     })
+      console.log('23'
+          // {
+          //   name: this.modalSecond.name,
+          //   email: this.modalSecond.email
+          // }
+      )
+      //reset
+      // this.modalSecond.name = '';
+      // this.modalSecond.email = '';
+      // this.modalSecond.show = false
+    },
+    calcSizePayoff() {
+      let payoff = this.inputSalary()
+
+      this.createTablePayoff(payoff)
+      // this.arrPayoff = []
+    },
+    inputSalary() {
+      let sizeSalary = this.salary
+      let taxOfYear = (sizeSalary * 12) * 0.13
+      return taxOfYear
+    },
+    createTablePayoff(payoff) {
+      let blockHidden = document.querySelector('.hidden')
+      blockHidden.classList.remove('hidden')
+
+      let yearsOfPay = Math.ceil(maxSizePayoff / payoff)
+
+      let sum = 0
+      for (let i = 0; i <= yearsOfPay; ++i) {
+        let payoffObj = {
+          year: i + 1,
+          ending: '',
+          sumPayoff: ''
+        }
+        let year = i + 1
+        payoffObj.year = year
+        if (sum < maxSizePayoff) {
+          let differenceSum = maxSizePayoff - sum
+
+          if (differenceSum < payoff) {
+            payoffObj.sumPayoff = differenceSum
+            payoffObj.ending = this.declOfNum( year )
+            this.arrPayoff.push(payoffObj)
+            break
+          }
+        }
+        sum = sum + payoff
+
+        payoffObj.sumPayoff = payoff
+        payoffObj.ending = this.declOfNum( year )
+        this.arrPayoff.push(payoffObj)
+        console.log('arrPayoff', this.arrPayoff)
+      }
+    },
+    declOfNum(n) {
+      const endsOfWord = ['-ый', '-ой', '-ий']
+      if (n === 1 || n === 4 || n === 5 || (n > 9 && n < 20)) { return endsOfWord[0]; }
+      if (n === 2 || n > 5 && n < 9) { return endsOfWord[1]; }
+      if (n === 3) { return endsOfWord[2]; }
+      return endsOfWord[1];
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import './assets/main.sass'
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  font-size: 16px
+  height: 100vh
+  position: fixed
+  top: 0
+  bottom: 0
+  left: 0
+  right: 0
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <transition name="modal">
+      <div class="modal__wrapper" @click="$emit('close')">
+        <div class="modal-content" @click.stop="">
+          <!--  header-->
+          <div class="modal-header">
+            <span class="modal-title"> Налоговый вычет </span>
+            <span class="button-close" @click="$emit('close')">×</span>
+          </div>
+
+          <!-- body-->
+          <div class="modal-body">
+            <slot name="body">
+<!--              <input type="number">-->
+            </slot>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {}
+  },
+  mounted() {
+    document.body.addEventListener('keyup', e => {
+      if (e.key === 'escape') {
+        this.$emit('close')
+      }
+    })
+  },
+  computed: {},
+  methods: {}
+
+}
+</script>
+
+
+<style lang="sass" scoped>
+//animation
+.modal-enter .modal-leave-active
+  opacity: 0
+
+
+.modal-enter .modal-content,
+.modal-leave-active .modal-content
+  transform: scale(1.2)
+
+.modal__wrapper
+  display: flex
+  justify-content: center
+  align-items: center
+  position: fixed
+  top: 0
+  bottom: 0
+  left: 0
+  right: 0
+  transition: opacity .3s ease
+  z-index: 998
+  background-color: #999999
+
+.modal-content
+  position: relative
+  width: 453px
+  //height: 476px
+  padding: 32px
+  background-color: #fff
+  border: 1px solid #dcdfe6
+  transition: all .2s ease
+  border-radius: 30px
+  z-index: 999
+  overflow: hidden
+  @media screen and (min-width: 900px)
+    min-width: 500px
+
+.modal-header
+  display: flex
+  align-self: center
+  justify-content: space-between
+  margin-bottom: 16px
+  span
+    font-size: 24px
+  .button-close
+    cursor: pointer
+
+.modal-body
+  text-align: center
+
+.modal-title
+  font-weight: bold
+  font-size: 28px
+  line-height: 40px
+
+</style>
